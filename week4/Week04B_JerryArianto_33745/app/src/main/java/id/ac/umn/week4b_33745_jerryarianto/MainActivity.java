c5614c1636f14eb20c735ebc9bd4c9c903eaab3c
jryarianto <jerry.arianto@student.umn.ac.id>
2021-09-18 11:52:32 +0700

    apakek

---
+++ week4/Week04B_JerryArianto_33745/app/src/main/java/id/ac/umn/week4b_33745_jerryarianto/MainActivity.java
@@ -0,0 +1,34 @@
+package id.ac.umn.week4b_33745_jerryarianto;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button_change_1, button_change_2;
+    private EditText nama;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button_change_1 = findViewById(R.id.main_button_change_1);
+        button_change_2 = findViewById(R.id.main_button_change_2);
+
+        button_change_1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intentFirst = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intentFirst);
+            }
+        });
+        button_change_2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intentSecond = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intentSecond);
+            }
+        });
+    }
+}
---
+++ week4/Week04B_JerryArianto_33745/app/src/main/java/id/ac/umn/week4b_33745_jerryarianto/SecondActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.week4b_33745_jerryarianto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}   
